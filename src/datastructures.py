755306655e91fa923128aabf43f6c86db5ac4bdf
Felipe Moreno <85708817+blacklottus-09@users.noreply.github.com>
2025-06-02 21:35:23 +0000

    the excersice was solved

--- src/datastructures.py
+++ src/datastructures.py
@@ -16,8 +16,22 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
-            }
-        ]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10,14,3]   
+            },
+             {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]   
+            }]
+
 
     # This method generates a unique incremental ID
     def _generate_id(self):
@@ -26,20 +40,33 @@ class FamilyStructure:
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+
+        if "id" not in member:
+            member["id"] = self._generate_id()
+
+        if "first_name" not in member or "age" not in member or "lucky_numbers" not in member:
+            raise ValueError("Faltan campos requeridos: first_name, age, lucky_numbers")
+        
+        if not isinstance(member["age"], int) or member["age"] <= 0:
+            raise ValueError("El campo age debe ser un número entero positivo")
+
+        if not isinstance(member["lucky_numbers"], list) or not all(isinstance(n, int) for n in member["lucky_numbers"]):
+            raise ValueError("lucky_numbers debe ser una lista de enteros")        
+        self._members.append(member)
+    
+        return member
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        
+        member = [item for item in self._members if item["id"] == id]
+        if member:
+            self._members.remove(member[0])
+            return True
+        return False
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        
+        return next((item for item in self._members if item["id"] == id), [])
 
-    # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
